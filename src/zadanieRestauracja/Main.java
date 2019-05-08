27f5aa8a713a71c997a64e82195990a4c32181bb
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-05-08 19:37:18 +0200

    add MariuszS

--- src/zadanieRestauracja/Main.java
+++ src/zadanieRestauracja/Main.java
@@ -2,18 +2,18 @@ package zadanieRestauracja;
 
 public class Main {
     public static void main(String[] args) {
-    }
-    public void printGucio() {
-        System.out.println("Hello gucio");
+
     }
 
-    public void printMarcin() {
-        System.out.println("Hello, Marcin here");
+    public void printMariusz() {
+        System.out.println("Hello, MariuszS");
     }
 
 
-    public void printHubert(){ System.out.println("Hello, Hubert here");
-    }
+
+
+
+
 
 
 }
