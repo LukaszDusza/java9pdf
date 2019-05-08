4efbeb54f30bfd930a64b3e7b3fb4b0eb1299cdf
gucio <kurbaniak@gmail.com>
2019-05-08 19:37:39 +0200

    gucio clean project

--- src/zadanieRestauracja/Main.java
+++ src/zadanieRestauracja/Main.java
@@ -1,21 +1,9 @@
 package zadanieRestauracja;
 
 public class Main {
-   public void printLukasz() {
-       System.out.println("Hello Lukasz");
-   }
-    public static void main(String[] args) {
-    }
+
     public void printGucio() {
         System.out.println("Hello gucio");
-    }
-
-    public void printMarcin() {
-        System.out.println("Hello, Marcin here");
-    }
-
-
-    public void printHubert(){ System.out.println("Hello, Hubert here");
     }
 
 
