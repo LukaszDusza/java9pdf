21cb994b2bc215dac068cd88c0d54e059eca5d4e
gucio <kurbaniak@gmail.com>
2019-05-08 18:55:25 +0200

    psvm added by gucio

--- src/zadanieRestauracja/Main.java
+++ src/zadanieRestauracja/Main.java
@@ -1,4 +1,9 @@
 package zadanieRestauracja;
 
 public class Main {
+    public static void main(String[] args) {
+    }
+    public void printGucio() {
+        System.out.println("Hello gucio");
+    }
 }
