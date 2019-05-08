9a21f0ce0850b60e70b5efc5aff173a8c4a15a79
id github: Ade-X <m.jarosz82@gmail.com>
2019-05-08 19:39:25 +0200

    init mariusz-d branch

--- src/zadanieRestauracja/Main.java
+++ src/zadanieRestauracja/Main.java
@@ -1,26 +1,12 @@
 package zadanieRestauracja;
 
 public class Main {
-   public void printLukasz() {
-       System.out.println("Hello Lukasz");
-   }
+
     public static void main(String[] args) {
-    }
-    public void printGucio() {
-        System.out.println("Hello gucio");
     }
 
     public void printMariuszJ() {
         System.out.println("Hello MariuszJ :-)");
     }
-
-    public void printMarcin() {
-        System.out.println("Hello, Marcin here");
-    }
-
-
-    public void printHubert(){ System.out.println("Hello, Hubert here");
-    }
-
 
 }
